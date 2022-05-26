98b7d3699a057bf10bcad6862c3b9c69cb16b50c
KangOxford <kang.li@maths.ox.ac.uk>
2022-05-26 02:03:11 +0000

    done

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
+
+# from keras.preprocessing.image import load_img
+from keras.utils import load_img
+# from keras.preprocessing.image import img_to_array
+from keras.utils import img_to_array
 from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 from keras.applications.vgg16 import VGG16
@@ -22,12 +25,13 @@ def predict():
     image = load_img(image_path, target_size = (224,224))
     image = img_to_array(image)
     image = image.reshape((1,image.shape[0],image.shape[1],image.shape[2]))
-    image = preprocess_input(input)
+    image = preprocess_input(image)
     yhat  = model.predict(image)
     label = decode_predictions(yhat)
     label = label[0][0]
     classifcation = "%s (%.2f%%)" % (label[1], label[2]*100)
-    return render_template("index.html",predcition = classifcation)
+    return render_template("index.html",prediction = classifcation)
+    # return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(port = 3000, debug= True)
